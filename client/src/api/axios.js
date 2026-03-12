2482a14c18e7cf789a439bd91b6a24ce60948980
muhammad syaifulloh <skyfoxmarket@gmail.com>
2026-03-12 09:45:44 +0700

    Installasi dan dependecy website top up menggunakan react js dan laravel

---
+++ client/src/api/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    // Mengizinkan pengiriman cookie/token jika diperlukan nanti
+    withCredentials: true, 
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+});
+
+// Nanti kita bisa tambahkan Interceptor di sini untuk menyelipkan Token Sanctum
+
+export default api;
